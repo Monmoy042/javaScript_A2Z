c9046d62cbaf34afaa66d3d8a346c16b41b090b1
Monmoy042 <monmoykms.16@gmail.com>
2019-08-10 21:58:57 +0600

    01Basic.js file modified

--- 01Basic.js
+++ 01Basic.js
@@ -1,2 +1,11 @@
-// Show a string
+// Print a string
 console.log('Hello Bangladesh');
+
+// Print the addition of 2 numbers
+console.log(3+3);
+
+// Add two strings
+console.log('Hello'+' '+'World');
+
+//We can use single quote or double quote to declare a string
+console.log("Hello World Again!")
