31a2e00ddb5674480a9f2b46ca341ed6408df969
Monmoy042 <monmoykms.16@gmail.com>
2019-08-13 12:17:53 +0600

    Arithmetic operator in javaScript

---
+++ 04ArithmeticOperator.js
@@ -0,0 +1,20 @@
+// Arithmetic Operator in javaScript
+/*
+    1. Addition(+)
+    2. Subtraction(-)
+    3. Multiplication(*)
+    4. Division(/)
+*/
+
+var num1 = 128;
+var num2 = 64;
+
+var add = num1 + num2; // Addition of two numbers
+var sub = num1 - num2; // Subtraction of two numbers
+var mul = num1 * num2; // Multiplication of two numbers
+var div = num1 / num2; // Division of two numbers
+
+console.log('Addition of two numbers: '+add);
+console.log('Subtraction of two numbers: '+sub);
+console.log('Multiplication of two numbers: '+mul);
+console.log('Division of two numbers: '+div);
