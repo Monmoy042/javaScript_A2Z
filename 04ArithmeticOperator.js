96fe55f4836698ac634e781e34c0e524a0785c38
Monmoy042 <monmoykms.16@gmail.com>
2019-08-13 21:34:04 +0600

    04ArithmeticOperator.js file modified

--- 04ArithmeticOperator.js
+++ 04ArithmeticOperator.js
@@ -4,6 +4,9 @@
     2. Subtraction(-)
     3. Multiplication(*)
     4. Division(/)
+    5. Increment(++)
+    6. Decrement(--)
+    7. Modulus(%)
 */
 
 var num1 = 128;
@@ -18,3 +21,29 @@ console.log('Addition of two numbers: '+add);
 console.log('Subtraction of two numbers: '+sub);
 console.log('Multiplication of two numbers: '+mul);
 console.log('Division of two numbers: '+div);
+
+// Increment
+let x = 10;
+let y = ++x; // This is called prefix operation | y = x+1
+console.log(y);
+
+let p = 350;
+let q = p++;
+let r = p++; // This is called postfix operation | r = p + 1
+console.log(q);
+console.log(r);
+
+// Decrement
+let a = 105;
+let b = --a; // This is called prefix operation | b = a - 1
+console.log(b);
+
+let l = 550;
+let m = l--;
+let n = l--; // This is called postfix operation | n = l - 1
+console.log(m);
+console.log(n);
+
+// Modulus
+let mod = num1 % 3;
+console.log(mod); // The remainder is 2
