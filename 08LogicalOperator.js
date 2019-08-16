d4fe158258a97e4f56bdfed77703648336c38dcf
Monmoy042 <monmoykms.16@gmail.com>
2019-08-16 11:30:30 +0600

    08LogicalOperator.js file added

---
+++ 08LogicalOperator.js
@@ -0,0 +1,15 @@
+// Logical Operator in JavaScript
+/*
+    Operator    Description
+       ||       Logical OR
+       &&       Logical AND
+       !        Logical NOT
+*/
+
+// Logical OR
+console.log(false||true); // Output will be true.
+
+let x = 10;
+let y = 20;
+let z = x || y;
+console.log(z)
