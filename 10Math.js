483a1973ae15d834a8470fd2b25e95a376b0be31
Monmoy042 <monmoykms.16@gmail.com>
2019-08-16 20:06:18 +0600

    10Math.js file added

---
+++ 10Math.js
@@ -0,0 +1,43 @@
+// Math Object in javaScript
+
+/*
+    01. Math.PI --> Ratio of the circumference of a circle to its diameter, approximately 3.14159
+    02. Math.E --> Euler's constant
+    03. Math.LN2 --> Natural logarithm of 2, approximately 0.693
+    04. Math.LN10 --> Natural logarithm of 10, approximately 2.303
+    05. Math.LOG2E --> Base 2 logarithm of E, approximately 1.443
+    06. Math.LOG10E --> Base 10 logarithm of E, approximately 0.434
+    07. Math.SQRT1_2 --> Square root of 1/2
+    08. Math.SQRT2 --> Square root of 2 
+    09. Math.abs(x) --> Absolute value of x
+    10. Math.ceil(x) --> 
+    11. Math.cbrt(x) -->
+    12. Math.exp(x) -->
+    13. Math.floor(x) -->
+    14. Math.fround(x) -->
+    15. Math.log(x) -->
+    16. Math.max(x,y,z,....) --> 
+    17. Math.min(x,y,z,....) -->
+    18. Math.pow(x,y) --> 
+    19. Math.random() -->
+    20. Math.round(x) --> 
+    21. Math.sign(x) --> 
+    22. Math.sin(x) --> 
+    23. Math.sqrt(x) -->
+    24. Math.tan(x) -->
+    25. 
+*/
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log('The value of LN2 is: ' + Math.LN2);
+console.log('The value of LN10 is: ' + Math.LN10);
+console.log('The value of LOG2E is: ' + Math.LOG2E);
+console.log('The value of LOG10E is: ' + Math.LOG10E);
+console.log('The value of square root of 1/2 is: ' + Math.SQRT1_2);
+console.log('The value of square root of 2 is: ' + Math.SQRT2);
+console.log('The value of square root of 2 is: ' + Math.sqrt(1));
+
+
+
+
