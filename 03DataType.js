09c3735ee5e286d13766398be5a1226a9918d498
Monmoy042 <monmoykms.16@gmail.com>
2019-08-19 20:36:18 +0600

    03DataType.js file modified

--- 03DataType.js
+++ 03DataType.js
@@ -37,7 +37,7 @@ const product={
     productName: 'Arduino',
     productID: 300150,
     productVersion: 'R3',
-    price: 450 + 'BDT'
+    price: 450 + 'BDT',
 };
 console.log(product.price);
 
@@ -52,6 +52,13 @@ console.log(typeof(bool1));
 var bool2 = false;
 console.log(typeof(bool2));
 
+console.log(Boolean(3>1));
+
+var bool3 = 0; // In boolean 0 consider as false state
+console.log(Boolean(bool3));
+var bool4 = 1; // In boolean 1 consider as true state
+console.log(Boolean(bool4));
+
 // Undefined
 var u;
 console.log(typeof(u));
@@ -59,3 +66,23 @@ console.log(typeof(u));
 // Null
 var v = null;
 console.log(typeof(v));
+
+// NaN --> Not a Number
+// The NaN property represents "Not-a-Number" value. This property indicates that a value is not a legal number.
+
+var n = 10;
+var m = n/"name";
+console.log(m);
+console.log(isNaN(bool3)); //Check NanN or not
+
+var sub1 = "Mathematics";
+var sub2 = "English";
+var value = sub1*sub2;
+console.log(value);
+console.log(isNaN(value));
+
+var a = 10;
+var b = '10';
+var c = a+b;
+console.log(c);
+console.log(isNaN(c));
