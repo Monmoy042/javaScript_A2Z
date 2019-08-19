a418a0562cfae5ba90c4f7d029da17ebd84317a5
Monmoy042 <monmoykms.16@gmail.com>
2019-08-19 20:43:05 +0600

    10Math.js file modified

--- 10Math.js
+++ 10Math.js
@@ -11,25 +11,24 @@
     08. Math.SQRT2 --> Square root of 2 
     09. Math.abs(x) --> Absolute value of x
     10. Math.ceil(x) --> 
-    11. Math.cbrt(x) -->
-    12. Math.exp(x) -->
+    11. Math.cbrt(x) --> Cube root of x
+    12. Math.exp(x) --> 
     13. Math.floor(x) -->
     14. Math.fround(x) -->
     15. Math.log(x) -->
-    16. Math.max(x,y,z,....) --> 
-    17. Math.min(x,y,z,....) -->
-    18. Math.pow(x,y) --> 
-    19. Math.random() -->
+    16. Math.max(x,y,z,....) --> Return Maximum value between x,y,z and more
+    17. Math.min(x,y,z,....) --> Return Minimum value between x,y,z and more
+    18. Math.pow(x,y) --> Return x to the power of y
+    19. Math.random() --> 
     20. Math.round(x) --> 
     21. Math.sign(x) --> 
     22. Math.sin(x) --> 
-    23. Math.sqrt(x) -->
-    24. Math.tan(x) -->
-    25. 
+    23. Math.sqrt(x) --> Return square root of x 
+    24. Math.tan(x) --> 
 */
 
-console.log(Math.PI);
-console.log(Math.E);
+console.log('The value of pi is: ' + Math.PI);
+console.log("Euler's constant: " + Math.E);
 console.log('The value of LN2 is: ' + Math.LN2);
 console.log('The value of LN10 is: ' + Math.LN10);
 console.log('The value of LOG2E is: ' + Math.LOG2E);
@@ -37,7 +36,3 @@ console.log('The value of LOG10E is: ' + Math.LOG10E);
 console.log('The value of square root of 1/2 is: ' + Math.SQRT1_2);
 console.log('The value of square root of 2 is: ' + Math.SQRT2);
 console.log('The value of square root of 2 is: ' + Math.sqrt(1));
-
-
-
-
