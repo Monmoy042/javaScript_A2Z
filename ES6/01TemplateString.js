032b657d08a3fba6a5a3e3a73099167d5477d6ef
Monmoy042 <monmoykms.16@gmail.com>
2019-08-29 22:44:09 +0600

    01TemplateString.js file has modified

---
+++ ES6/01TemplateString.js
@@ -0,0 +1,14 @@
+// Template Strings
+var num1 = 120;
+console.log(`The value of num1 is ${num1}`);
+
+var num2 = 320;
+console.log(`The addition of num1 and num2 is ${num1+num2}`);
+
+console.log('\n');
+
+const student =`Name: John
+Age 16
+class 10
+10 plus 20 is: ${10+20}`;
+console.log(student);
