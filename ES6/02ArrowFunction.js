c6ac7f9a5c94bdd3116036fb6083e3b2840bb6e8
Monmoy042 <monmoykms.16@gmail.com>
2019-08-29 22:55:53 +0600

    Arrow function in javaScript

--- ES6/02ArrowFunction.js
+++ ES6/02ArrowFunction.js
@@ -1,1 +1,14 @@
 // Arrow Function in javaScript
+const sayHello=()=>console.log('Hello World!');
+sayHello();
+
+const sqr =(x)=>{
+    return x*x;
+};
+console.log(`Two square is: ${sqr(2)}`);
+
+const minimum = (p,q,r,s)=>{
+    let min = Math.min(p,q,r,s);
+    return min;
+}
+console.log(`The minimum number is: ${minimum(25,8,63,75)}`);
