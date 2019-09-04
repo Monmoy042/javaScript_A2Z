1b023d3543252f1804a43b53a14c8df644df8d70
Monmoy042 <monmoykms.16@gmail.com>
2019-09-04 21:21:22 +0600

    javaScript on the web

--- Basic/script.js
+++ Basic/script.js
@@ -1,3 +1,21 @@
-// console.log('Hello World!');
-document.write('Hello Bangladesh!');
+// Show an alert message
+alert('Helo World');
+window.alert('Hello There');
+
+// It is also used for showing the output
+// document.write('Bangladesh!');
+
+// To access HTML elements using document.getElementByID('ID')
+document.getElementById("first").innerHTML = "My First JavaScript";
+document.getElementById('add').innerHTML = 3+6;
+var name = document.getElementById('name').value='Monmoy';
+alert(name); 
+
+// Console log --> show the output to the console 
+var a = 10;
+var b = 20;
+var ans = a+b;
+console.log('\n');
+console.log('The answer of a+b is: ' + ans);
+
 
