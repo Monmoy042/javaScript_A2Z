9ba85c2b8f6aaf22b9625504252fbf0a87dfc127
Monmoy042 <monmoykms.16@gmail.com>
2019-09-06 20:32:23 +0600

    More examples added

--- Basic/16Example.js
+++ Basic/16Example.js
@@ -2,6 +2,7 @@
 // 1. Find out the even and odd number using javaScript
 // 2. Find out the maximum and minimum numbers using a javaScript user define function
 // 3. Fibonacci series up to 20
+// 4. Swap two numbers
 
 // 1. Identify the Even and Odd Numbers
 let a = 35;
@@ -21,3 +22,17 @@ var maxMin = function(a,b,c,d,e){
 maxMin(25,92,36,885,47);
 
 // 3. Fibonacci Series find out using javaScript
+
+// 4. Swap two numbers using temporary variable and without temporary variable
+var n = 10;
+var m = 20;
+console.log(`Before swapping:The value of n=${n} and m=${m}`);
+
+// var temp = 0;
+// temp = n;
+// n = m;
+// m = temp;
+
+[n,m]=[m,n];
+console.log(`After swapping:The value of n=${n} and m=${m}`);
+
