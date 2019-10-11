2f15ff892adfec92c0c495955cc7305bd09f343c
Monmoy042 <monmoykms.16@gmail.com>
2019-10-11 20:42:29 +0600

    08LogicalOperator.js file modified

--- Basic/08LogicalOperator.js
+++ Basic/08LogicalOperator.js
@@ -8,8 +8,18 @@
 
 // Logical OR
 console.log(false||true); // Output will be true.
-
 let x = 10;
 let y = 20;
 let z = x || y;
 console.log(z)
+
+// Logical AND
+console.log(false && true);
+let a = -2;
+let b = 2;
+console.log(a>0 && a<2); // Output will be false
+
+// Logical NOT
+let n1 = 10;
+console.log(n1!=20);
+console.log(n1 !=10);
