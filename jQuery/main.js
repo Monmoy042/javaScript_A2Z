eb65e566417aed55c1a87fb6bc3230d4ab0918c2
Monmoy042 <monmoykms.16@gmail.com>
2019-12-21 21:12:56 +0600

    Added jQuery

---
+++ jQuery/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    // alert(1);
+    // document.write("Hello World!");
+    console.log('Hello World');
+    var a = $('#test1').html();
+    console.log(a);
+
+    // Basic Selector
+    $('#test2').css('color', 'black');
+    // $('li:first-child').css('border','1px red solid');
+    // $('li:first').css('border','1px red solid');
+    // $('li:last').css('border','1px red solid');
+    // $('ul li:gt(2)').css('color','red');
+    // $('ul li:lt(2)').css('color','green');
+    // $('ul li:eq(3)').css('color','green');
+    // $('ul li:even()').css('color','green');
+    // $('ul li:odd()').css('color','green');
+
+    // Mouse Event
+    $('.button1').click(function () {
+        $('.mouseEventP').css('color', 'red');
+    });
+
+    $('.button1').dblclick(function () {
+        $('.mouseEventP').css('color', 'green');
+    });
+
+    $('.button1').contextmenu(function () {
+        $('.mouseEventP').css('color', 'brown');
+    });
+
+    $('.button1').mouseenter(function () {
+        $('.mouseEventP').css('color', 'purple');
+    });
+
+    $('.button1').mouseleave(function () {
+        $('.mouseEventP').css('color', 'blue');
+    });
+});
