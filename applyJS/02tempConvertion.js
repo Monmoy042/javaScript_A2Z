79bff9c28f5970616f58f297963f374dd425672a
Monmoy042 <monmoykms.16@gmail.com>
2020-08-09 21:49:53 +0600

    Temperature convertion

---
+++ applyJS/02tempConvertion.js
@@ -0,0 +1,9 @@
+// Fahrenheit to Degree Celsius
+let tempConvert = function(f){
+    let deg = (f-32)/1.8;
+    return deg;
+}
+let tempFarToDeg = tempConvert(100);
+console.log(tempFarToDeg);
+
+// Degree Celsius to Fahrenheit
