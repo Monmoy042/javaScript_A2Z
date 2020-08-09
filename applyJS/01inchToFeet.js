54a6eeaf8d9e70053b1da9971c478e5f3c15a406
Monmoy042 <monmoykms.16@gmail.com>
2020-08-09 20:43:31 +0600

    Apply JS in practical life

---
+++ applyJS/01inchToFeet.js
@@ -0,0 +1,17 @@
+
+var inchToFeet = function(inch){
+    var feet = inch/12;
+    return feet;
+} 
+
+var finalResult = inchToFeet(60);
+console.log(finalResult);
+
+
+// Feet to inch
+var feetToInch = function(feet2){
+    var inch2 = 12*feet2;
+    return inch2;
+}
+var convertFeetToInch = feetToInch(5);
+console.log(convertFeetToInch);
