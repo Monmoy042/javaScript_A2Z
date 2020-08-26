d9ac65a0718991b9320638184907c65eb88ade4f
Monmoy042 <monmoykms.16@gmail.com>
2020-08-26 21:28:48 +0600

    Target the HTML elements using DOM manipulation

---
+++ DOM/main.js
@@ -0,0 +1,32 @@
+// Dom manipulation
+// Target the HTML elements
+/*
+########### Basic Methods####################
+var element;
+element = document;
+element = document.all;
+element = document.all[6];
+element = document.title;
+element = document.body;
+element = document.links; //For showing how many anchor or links are available
+element = document.links[0]; 
+element = document.images; 
+element = document.forms;
+element = document.doctype;
+element = document.URL;
+element = document.domain;
+element = document.baseURI;
+console.log(element);
+
+################# Target ID, class and tag ###################
+element = document.getElementById('hed');
+element = document.getElementsByClassName('text');
+element = document.getElementsByTagName('p');
+element = document.getElementsByTagName('p')[0];
+console.log(element);
+*/
+
+
+
+
+
