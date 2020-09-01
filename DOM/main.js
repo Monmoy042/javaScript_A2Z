de3e476dff2967dabb1530b24b0be8ccd51e07c0
Monmoy042 <monmoykms.16@gmail.com>
2020-09-01 21:40:50 +0600

    DOM manipulation added

--- DOM/main.js
+++ DOM/main.js
@@ -27,7 +27,7 @@ console.log(element);
 */
 
 /*
-########### Set value and Get value############//#
+########### Set value and Get value#############
 What we can get with DOM?
 1) HTML
 2) Text
@@ -40,7 +40,49 @@ C. getAttribute
 D. getAttributeNode
 E. Attribute
 */
-var h1Text;
-// h1Text = document.getElementById('newText').innerText;
-h1Text = document.getElementById('newText').innerHTML;
-console.log(h1Text);
+// var text = document.getElementById('text').innerText;
+// console.log(text);
+// var h1Text = document.getElementById("newText").innerHTML = "<h1>Monmoy</h1>";
+// console.log(h1Text);
+
+// ########### Query Selector #############
+// var text2 = document.querySelector('#text2').innerHTML="This is a simple text";
+// console.log(text2);
+// var text3 = document.querySelectorAll('.list');
+// console.log(text3);
+// var text4 = document.querySelectorAll('.list').innerHTML = '<p>Pokemon</p>'
+// console.log(text4);
+
+/*
+    Style using DOM
+*/
+// var colorChange = document.querySelector('#head').style.color="red";
+// console.log(colorChange);
+// document.querySelector("#head").style.backgroundColor="black";
+
+// var newClassName = document.querySelector("#head").className='abc';
+// console.log(newClassName);
+
+/*
+    Add eventListener
+*/
+// var clickEvent = document.querySelector('#clickMe').onclick = f1;
+// function f1(){
+//     document.getElementById('clickMe').style.color="orange";
+// }
+// document.getElementById('login').onmouseleave=f2;
+// function f2(){
+//     document.getElementById('login').style.color="green";
+// }
+
+document.getElementById("login").addEventListener("click", loginStyle);
+document.getElementById("login").addEventListener("dblclick", loginStyle2);
+function loginStyle() {
+  document.getElementById("login").style.color = "tomato";
+}
+
+function loginStyle2() {
+  document.getElementById("login").style.border = "1px solid red";
+  // document.getElementById("login").style.borderRadius='15px';
+  this.style.borderRadius = "15px";
+}
